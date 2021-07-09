fab5db1112c3661c27aa338a746d5763e80ff370
Arjen73 <arjendekivet@gmail.com>
2021-07-09 15:40:37 +0200

    Formatting

--- dbMongo.mjs
+++ dbMongo.mjs
@@ -1,26 +1,26 @@
-import { strictEqual } from 'assert';
+import { strictEqual } from 'assert'
 
 // mongodb is a CommonJS module.
-import moduleMongodb from 'mongodb';
-const { MongoClient } = moduleMongodb;
+import moduleMongodb from 'mongodb'
+const { MongoClient } = moduleMongodb
 
-const connectionUri = "mongodb://localhost:27017";
-let client;
+const connectionUri = 'mongodb://localhost:27017'
+let client
 
 export async function clientConnect() {
-  return (
-
-    client = await (() => (new Promise((resolve, reject) => (
-
-      MongoClient.connect(connectionUri, { useUnifiedTopology: true },
+  return (client = await (() =>
+    new Promise((resolve, reject) =>
+      MongoClient.connect(
+        connectionUri,
+        { useUnifiedTopology: true },
         (err, client) => {
-          strictEqual(null, err);
-          resolve(client);
-        })
-    )
-    )))());
+          strictEqual(null, err)
+          resolve(client)
+        }
+      )
+    ))())
 }
 export async function clientClose(client) {
-  client.close();
-  return true;
+  client.close()
+  return true
 }
--- server.mjs
+++ server.mjs
@@ -1,14 +1,16 @@
-import express from "express";
-const app = express();
-import { getEvents } from './routeEvents.mjs';
+import express from 'express'
+const app = express()
+import { getEvents } from './routeEvents.mjs'
 
 app.listen(3001, function () {
-  console.log('Server started on port 3001');
-  app.get("/events", (req, res) => {
-    getEvents().then(data => {
-      res.send(data);    
-    }).catch(e => {
-       next(e);
-    });
-  });
-});
+  console.log('Server started on port 3001')
+  app.get('/events', (req, res) => {
+    getEvents()
+      .then((data) => {
+        res.send(data)
+      })
+      .catch((e) => {
+        next(e)
+      })
+  })
+})
