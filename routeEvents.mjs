c593bfe201a2ef853f2dc82e35688b3cd6f15ff9
Arjen73 <arjendekivet@gmail.com>
2021-07-09 15:13:07 +0200

    Formatting

--- routeEvents.mjs
+++ routeEvents.mjs
@@ -1,19 +1,18 @@
 import { clientConnect, clientClose } from './dbMongo.mjs';
 
 export async function getEvents(username) {
-  return (await (() => (
-    new Promise((resolve, reject) => (clientConnect().then(client => {
-      //Make the query...
-      client
-        .db('myNewDatabase')
-        .collection('events')
-        .find({})
-        .toArray(
-          (err, data) => {
+  return await (() =>
+    new Promise((resolve, reject) =>
+      clientConnect().then((client) => {
+        //Make the query...
+        client
+          .db('myNewDatabase')
+          .collection('events')
+          .find({})
+          .toArray((err, data) => {
             clientClose(client);
             resolve(data);
-          }
-        );
-    })
-    ))))());
-} 
+          });
+      })
+    ))();
+}
