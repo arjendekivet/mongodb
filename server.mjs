d5faa8dcb62732c42f4b5b78e1b42c034d80ec18
Arjen73 <arjendekivet@gmail.com>
2021-07-21 10:56:57 +0200

    Change port number

--- server.mjs
+++ server.mjs
@@ -2,8 +2,8 @@ import express from 'express'
 const app = express()
 import { getEvents } from './routeEvents.mjs'
 
-app.listen(3001, function () {
-  console.log('Server started on port 3001')
+app.listen(4321, function () {
+  console.log('Server started on port 4321')
   app.get('/events', (req, res) => {
     getEvents()
       .then((data) => {
