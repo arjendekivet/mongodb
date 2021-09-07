ea6803f5985066e37f788542e11d0eeeb10d1b02
Arjen73 <arjendekivet@gmail.com>
2021-09-07 12:12:27 +0200

    Add routes

--- routeEvents.mjs
+++ routeEvents.mjs
@@ -4,11 +4,30 @@ export async function getEvents(username) {
   return await (() =>
     new Promise((resolve, reject) =>
       clientConnect().then((client) => {
-        //Make the query...
         client
           .db('myNewDatabase')
           .collection('events')
           .find({})
+          .toArray((err, data) => {
+            if (err) {
+              reject(err)
+            } else {
+              clientClose(client)
+              resolve(data)
+            }
+          })
+      })
+    ))()
+}
+
+export async function getEventById(id) {
+  return await (() =>
+    new Promise((resolve, reject) =>
+      clientConnect().then((client) => {
+        client
+          .db('myNewDatabase')
+          .collection('events')
+          .find({ id: id })
           .toArray((err, data) => {
             if (err) {
               reject(err)
--- server.mjs
+++ server.mjs
@@ -1,6 +1,6 @@
 import express from 'express'
 const app = express()
-import { getEvents } from './routeEvents.mjs'
+import { getEvents, getEventById } from './routeEvents.mjs'
 
 app.listen(4321, function () {
   console.log('Server started on port 4321')
@@ -12,5 +12,21 @@ app.listen(4321, function () {
       .catch((e) => {
         next(e)
       })
+  })
+
+  app.get('/event/:id', (req, res) => {
+    const intId = parseInt(req.params.id, 10)
+    getEventById(intId)
+      .then((data) => {
+        res.send(data)
+      })
+      .catch((e) => {
+        next(e)
+      })
+  })
+
+  app.post('/question', (req, res) => {
+    const { comment } = req.body
+    res.send({ html: marked(comment.trim()) })
   })
 })
