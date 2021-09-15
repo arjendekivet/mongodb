6dd16fe42519c3a9b1584a1e245e587f772f8d98
Arjen73 <arjendekivet@gmail.com>
2021-09-15 16:08:40 +0200

    Add version and timestamps

--- models/question.mjs
+++ models/question.mjs
@@ -2,21 +2,44 @@ import mongoose from 'mongoose'
 const Schema = mongoose.Schema
 
 // create student schema & model
-const QuestionSchema = new Schema({
-  title: {
-    type: String,
-    required: [true, 'Title field is required'],
+const QuestionSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: [true, 'Title field is required'],
+    },
+    type: {
+      type: String,
+    },
+    description: {
+      type: String,
+    },
+    created: {
+      type: Date,
+      default: Date.now,
+    },
   },
-  type: {
-    type: String,
-  },
-  description: {
-    type: String,
-  },
-  created: {
-    type: Date,
-    default: Date.now,
-  },
+  {
+    timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' },
+  }
+)
+
+QuestionSchema.pre('findOneAndUpdate', function () {
+  const update = this.getUpdate()
+  if (update.__v != null) {
+    delete update.__v
+  }
+  const keys = ['$set', '$setOnInsert']
+  for (const key of keys) {
+    if (update[key] != null && update[key].__v != null) {
+      delete update[key].__v
+      if (Object.keys(update[key]).length === 0) {
+        delete update[key]
+      }
+    }
+  }
+  update.$inc = update.$inc || {}
+  update.$inc.__v = 1
 })
 
 const Question = mongoose.model('questions', QuestionSchema)
